6dbc1a0166ac194a236b864d20786bc4d96c880a
Jonay Medina <jmedina@tribalworldwide.gt>
2025-01-15 18:36:57 -0400

    :hammer: reorder

--- internal/cors/cors.go
+++ internal/cors/cors.go
@@ -7,6 +7,7 @@ func Default() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Content-Type", "application/json")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
